9dcb2c1cfa888870ba234e72c0e4744c54579c56
jonp182015 <jonp182015@gmail.com>
2019-02-07 13:01:35 -0500

    added content to bamazonSchema.sql file

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -12,3 +12,32 @@ CREATE TABLE products(
   stock_quantity INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1, 'rubiks cube', 'toys', 9.00, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (2, 'mcbook pro 15"', 'electronics', 900.00, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3, 'uline army back pack', 'outdoors', 79.00, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4, 'jabra elite active 65t', 'electronics', 151.99, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5, 'instant pot 7-in-1 multi-cooker', 'appliances', 79.95, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (6, 'echo dot', 'electronics', 49.99, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (7, 'fire tv stick 4k', 'electronics', 39.99, 100);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (8, 'fire hd 8 tablet with Alexa', 'electronics', 49.99, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (9, 'if you can read this bring me wine socks', 'clothes', 11.89, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (10, 'hasbro conncet 4 game', 'toys', 9.99, 100);
